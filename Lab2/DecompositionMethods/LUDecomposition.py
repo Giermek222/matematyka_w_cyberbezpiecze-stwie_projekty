dd335b0d05c4fbce60afd229a4e8f0e0f406c883
janrudkowski <janek.rudkowski@wp.pl>
2024-03-03 22:34:08 +0100

    add LUdecomposition

---
+++ Lab2/DecompositionMethods/LUDecomposition.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+def ludecomposition(matrix):
+    n = matrix.shape[0]
+    L = np.eye(n, dtype = int)
+    U = np.zeros((matrix.shape[0], matrix.shape[1]), dtype= int)
+
+    for i in range(n):
+        for j in range(i,n):
+            s=0
+            for k in range(i):
+                s += (L[i,k]*U[k,j])
+            U[i,j]=matrix[i,j]-s
+
+        for j in range(i+1,n):
+            s=0
+            for k in range(i):
+                s += (L[j,k]*U[k,i])
+            L[j,i]=((1/U[i,i])*(matrix[j,i]-s))
+
+    return L,U
