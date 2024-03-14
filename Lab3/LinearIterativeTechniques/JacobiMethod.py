4c566c2285127924a05f626737a773275f756d98
WFS\buchwald <wojciech.buchwald@webfleet.com>
2024-03-14 20:17:15 +0100

    fix method

--- Lab3/LinearIterativeTechniques/JacobiMethod.py
+++ Lab3/LinearIterativeTechniques/JacobiMethod.py
@@ -3,7 +3,7 @@ import numpy as np
 def jacobi_method(A, b, x0, tol=1e-6, max_iter=1000):
     n = len(b)
     x = np.float64(x0.copy()) 
-    x_new = np.float64(np.zeros_like(x))
+    x_new = np.float64(np.zeros_like(x0))
     iterations = 0
     
     while iterations < max_iter:
@@ -16,6 +16,7 @@ def jacobi_method(A, b, x0, tol=1e-6, max_iter=1000):
             
         if np.linalg.norm(x_new - x) < tol:
             break
+        x = x_new.copy()
         iterations += 1
     return x_new
 
